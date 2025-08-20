3fd321d3fae2161be0f64ed1ff52f9865ef6326c
Carolina Sasowsky <mc.sasowsky@gmail.com>
2025-08-20 14:25:54 -0300

    feat(menu): ajustar menú hamburguesa en versión móvil
    - Se corrigió la visibilidad del menú al desplegarse en dispositivos móviles.
    - Se añadió funcionalidad para cerrar el menú con un botón 'X'.
    - Se centró el texto del menú y se restauró la alineación del logo.
    - Mejoras generales en la experiencia de navegación móvil.

--- src/components/MenuTop/MenuTop.jsx
+++ src/components/MenuTop/MenuTop.jsx
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Menu } from "antd";
+import { MenuOutlined, CloseOutlined } from "@ant-design/icons";
 import "./MenuTop.sass";
 
 const MenuTop = () => {
+	const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+	const toggleMenu = () => {
+		setIsMobileMenuOpen(!isMobileMenuOpen);
+	};
+
 	return (
 		<div className="menu-top">
+			{/* Logo */}
 			<div className="menu-top__logo">
 				<div className="logo2"></div>
 			</div>
+
+			{/* Botón hamburguesa / cerrar */}
+			<div className="menu-top__toggle" onClick={toggleMenu}>
+				{isMobileMenuOpen ? <CloseOutlined /> : <MenuOutlined />}
+			</div>
+
+			{/* Menú de escritorio */}
 			<Menu
 				theme="dark"
 				mode="horizontal"
 				defaultSelectedKeys={["1"]}
+				className="menu-desktop"
 				style={{ lineHeight: "68px" }}
 			>
 				<Menu.Item key="1">
@@ -29,6 +45,39 @@ const MenuTop = () => {
 				</Menu.Item>
 				<Menu.Item key="5">
 					<Link to="/favorites">Favoritos</Link>
+				</Menu.Item>
+			</Menu>
+
+			{/* Menú móvil (slide) */}
+			<Menu
+				theme="dark"
+				mode="vertical"
+				className={`menu-mobile ${isMobileMenuOpen ? "open" : ""}`}
+			>
+				<Menu.Item key="1">
+					<Link to="/" onClick={toggleMenu}>
+						Home
+					</Link>
+				</Menu.Item>
+				<Menu.Item key="2">
+					<Link to="/new-movies" onClick={toggleMenu}>
+						Últimos lanzamientos
+					</Link>
+				</Menu.Item>
+				<Menu.Item key="3">
+					<Link to="/popular" onClick={toggleMenu}>
+						Más populares
+					</Link>
+				</Menu.Item>
+				<Menu.Item key="4">
+					<Link to="/search" onClick={toggleMenu}>
+						Buscador
+					</Link>
+				</Menu.Item>
+				<Menu.Item key="5">
+					<Link to="/favorites" onClick={toggleMenu}>
+						Favoritos
+					</Link>
 				</Menu.Item>
 			</Menu>
 		</div>
